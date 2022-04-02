c34381fc69bc39f5fb3ffedd0b4e7edf1f8e9c78
Jere Kuusela <jere.kuusela@iki.fi>
2022-04-02 12:05:21 +0300

    Refactoring.

--- Ruler/Service/MessageHud.cs
+++ Ruler/Service/MessageHud.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 namespace Service {
   // Prepends a custom message to the hud.
-  [HarmonyPatch(typeof(MessageHud), "Update")]
+  [HarmonyPatch(typeof(MessageHud), nameof(MessageHud.Update))]
   public class MessageHud_UpdateMessage : MonoBehaviour {
     public static Func<List<string>> GetMessage = () => new() { "Default message. Overwrite this with own function." };
-    public static void Postfix(MessageHud __instance) {
+    static void Postfix(MessageHud __instance) {
       // Wait for the game to load.
       if (Player.m_localPlayer == null) return;
       if (Hud.IsUserHidden()) return;
@@ -36,13 +36,13 @@ namespace Service {
   }
   // Track message change to ensure a clean slate in every update.
   // This allows multiple mods to work with the hud.
-  [HarmonyPatch(typeof(MessageHud), "UpdateMessage")]
+  [HarmonyPatch(typeof(MessageHud), nameof(MessageHud.UpdateMessage))]
   public class MessageHud_GetBaseMessage : MonoBehaviour {
     private static string BaseMessage = "";
-    public static void Prefix(out string __state) {
+    static void Prefix(out string __state) {
       __state = MessageHud.instance.m_messageText.text;
     }
-    public static void Postfix(MessageHud __instance, float ___m_msgQueueTimer, string __state) {
+    static void Postfix(MessageHud __instance, float ___m_msgQueueTimer, string __state) {
       if (__instance.m_messageText.text != __state)
         BaseMessage = __instance.m_messageText.text;
       // No more base game messages.
